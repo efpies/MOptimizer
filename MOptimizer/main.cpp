c45adaff55843d3512fd13b3666dc63a5c5e2765
efpies <efpies@gmail.com>
2013-03-03 04:55:16 +0400

    Initial Commit

---
+++ MOptimizer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MOptimizer
+//
+//  Created by Nicholas Yasnov on 03.03.13.
+//  Copyright (c) 2013 efpies. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
