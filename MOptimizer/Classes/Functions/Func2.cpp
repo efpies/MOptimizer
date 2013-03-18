9f2c276e7a40d2f67b0c8241ba9b793e8c5bd007
efpies <efpies@gmail.com>
2013-03-18 04:41:43 +0400

    Функция F = x1^2 + 3*x2^2 + 2*x1*x2

---
+++ MOptimizer/Classes/Functions/Func2.cpp
@@ -0,0 +1,33 @@
+//
+//  Func2.cpp
+//  MOptimizer
+//
+//  Created by Nicholas Yasnov on 18.03.13.
+//  Copyright (c) 2013 efpies. All rights reserved.
+//
+
+#include "Func2.h"
+
+#include "Point.h"
+
+#define SQ(x) (x) * (x)
+#define QB(x) (x) * (x)
+
+double Func2::value(const Point &point) const
+{
+    if (point.dimensions() != dimensions()) {
+        throw new IFunction::DimensionException;
+    }
+    
+    return SQ(point[0]) + 3 * SQ(point[1]) + 2 * point[0] * point[1];
+}
+
+short Func2::dimensions() const
+{
+    return 2;
+}
+
+string Func2::stringPresentation() const
+{
+    return string("F = x1^2 + 3*x2^2 + 2*x1*x2");
+}
---
+++ MOptimizer/Classes/Functions/Func2.h
@@ -0,0 +1,21 @@
+//
+//  Func2.h
+//  MOptimizer
+//
+//  Created by Nicholas Yasnov on 18.03.13.
+//  Copyright (c) 2013 efpies. All rights reserved.
+//
+
+#ifndef __MOptimizer__Func2__
+#define __MOptimizer__Func2__
+
+#include "IFunction.h"
+
+class Func2 : public IFunction {
+public:
+    double value(const Point &point) const;
+    short dimensions() const;
+    string stringPresentation() const;
+};
+
+#endif /* defined(__MOptimizer__Func2__) */
